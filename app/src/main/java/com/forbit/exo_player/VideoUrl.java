3b7307c1d706f0bb52c954412f08817d6fb78d2f
Saimulhoque <saimulhoque8214@gmail.com>
2020-12-22 12:25:46 +0600

    Update

---
+++ app/src/main/java/com/forbit/exo_player/VideoUrl.java
@@ -0,0 +1,18 @@
+package com.eminesa.erlabexoplayer.model;
+
+public class VideoUrl {
+
+    private String  mStringUrl;
+
+    public VideoUrl(String stringUrl) {
+        mStringUrl = stringUrl;
+    }
+
+    public String getStringUrl() {
+        return mStringUrl;
+    }
+
+    public void setStringUrl(String mStringUrl) {
+        this.mStringUrl = mStringUrl;
+    }
+}
